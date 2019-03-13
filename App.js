9120b0c5a8742974f12f17357653dc98e152ec4a
Danny Ou <danny.ou@smarkets.com>
2019-03-12 17:24:25 -0700

    Add firebase env

--- App.js
+++ App.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 import { View } from "react-native";
+import firebase from "@firebase/app";
+
+import {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId
+} from "react-native-dotenv";
 
 import Header from "./src/components/Header";
 import LoginForm from "./src/components/LoginForm";
 
 export default class App extends Component {
+  componentDidMount() {}
   render() {
     return (
       <View>
