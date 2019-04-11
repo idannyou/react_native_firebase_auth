363368fc3da77dcb9614ceb3781dbbed25b8ef40
Danny Ou <idannyou@gmail.com>
2019-04-10 22:45:34 -0700

    Corrected API key

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,25 +1,23 @@
 import * as firebase from 'firebase'
 
 import {
-  apiKey,
-  authDomain,
-  databaseURL,
-  projectId,
-  storageBucket,
-  messagingSenderId,
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
 } from 'react-native-dotenv'
 
 const config = {
-  apiKey,
-  authDomain,
-  databaseURL,
-  projectId,
-  storageBucket,
-  messagingSenderId,
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
 }
 
 firebase.initializeApp(config)
-
-console.log(firebase.app().options)
 
 export default firebase
