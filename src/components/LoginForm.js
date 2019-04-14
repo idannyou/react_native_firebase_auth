d9fc9fb1e933f00117d07fe4e71c13af8f404ea0
Danny Ou <idannyou@gmail.com>
2019-04-14 13:07:39 -0700

    add error handling

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react'
-import { View, Button } from 'react-native'
+import { View, Button, Text } from 'react-native'
 
 import Firebase from '../Firebase'
 
@@ -9,9 +9,11 @@ const CHANGE_EMAIL = 'CHANGE_EMAIL'
 const CHANGE_PASSWORD = 'CHANGE_PASSWORD'
 
 const SIGN_IN = 'SIGN_IN'
+const SIGN_IN_ERROR = 'SIGN_IN_ERROR'
+
 const SIGN_UP = 'SIGN_UP'
 
-const initialState = { email: '', password: '' }
+const initialState = { email: '', password: '', error: '' }
 
 function reducer(state, action) {
   switch (action.type) {
@@ -20,25 +22,53 @@ function reducer(state, action) {
     case CHANGE_PASSWORD:
       return { ...state, password: action.text }
     case SIGN_IN:
-      const { email, password } = state
-
-      try {
-        Firebase.auth().signInWithEmailAndPassword(email, password)
-      } catch (e) {
-        console.log(e)
-      }
-
       return { ...state }
+    case SIGN_IN_ERROR:
+      console.log('error', { action })
+      return { ...state, error: action.error }
     case SIGN_UP:
       return { ...state }
   }
 }
 
-const dispatchAction = (dispatch, type) => text => dispatch({ type, text })
+const handleOnChangeText = (dispatch, type) => text => dispatch({ type, text })
+
+const NOT_FOUND = 'NOT_FOUND'
+const ERROR_CODE = {
+  [NOT_FOUND]: 'auth/user-not-found',
+}
+
+handleSignInError = ({ dispatch, error }) => {
+  console.log(error)
+  switch (error.code) {
+    case ERROR_CODE[NOT_FOUND]:
+      dispatch({
+        type: SIGN_IN_ERROR,
+        error: 'User is not found. Please Sign Up.',
+      })
+      break
+    default:
+      dispatch({
+        type: SIGN_IN_ERROR,
+        error: 'General Error',
+      })
+      break
+  }
+}
+
+const handleSignIn = ({ dispatch, email, password }) => async () => {
+  try {
+    const payload = Firebase.auth().signInWithEmailAndPassword(email, password)
+    const resolvedPayload = await payload
+    console.log({ dispatch, email, password, resolvedPayload })
+  } catch (error) {
+    handleSignInError({ dispatch, error })
+  }
+}
 
 const LoginForm = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
-  const { email, password } = state
+  const { email, error, password } = state
   return (
     <View>
       <Input
@@ -46,17 +76,22 @@ const LoginForm = () => {
         placeholder="user@email.com"
         value={email}
         secureTextEntry={false}
-        onChangeText={dispatchAction(dispatch, CHANGE_EMAIL)}
+        onChangeText={handleOnChangeText(dispatch, CHANGE_EMAIL)}
       />
       <Input
         label="Password"
         placeholder="password"
         value={password}
         secureTextEntry={true}
-        onChangeText={dispatchAction(dispatch, CHANGE_PASSWORD)}
+        onChangeText={handleOnChangeText(dispatch, CHANGE_PASSWORD)}
       />
 
-      <Button title="Sign in" onPress={dispatchAction(dispatch, SIGN_IN)} />
+      <Button
+        title="Sign in"
+        onPress={handleSignIn({ dispatch, email, password })}
+      />
+
+      <Text>{error}</Text>
     </View>
   )
 }
